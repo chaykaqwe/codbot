574f4cda0f465dc064bf38e0a34377a0c7d709a7
chaykaqwe <tatardastan@gmail.com>
2025-04-10 21:35:44 +0300

    v2.0
    added check_subscription

--- hundlers.py
+++ hundlers.py
@@ -2,34 +2,45 @@ from aiogram import Router, F
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 import asyncio
+import os
+from dotenv import load_dotenv
+from aiogram.exceptions import TelegramBadRequest
+from app.bot import bot
 
-
-from app.database.reqiest import get_cods, get_random_ad
+from app.database.reqiest import get_cods, get_random_ad, get_sponsor, set_user
 import app.keybord as kb
 
 router = Router(name=__name__)
+load_dotenv()
+CHANNEL_USERNAME = os.getenv('CHANNEL_USERNAME')
 
 
 @router.message(CommandStart())
 async def cmd(mes: Message):
+    await set_user(mes.from_user.id)
     await mes.answer('Добро пожаловать! Введите код ')
     asyncio.create_task(send_advertisement(mes))
 
 
 @router.message(F.text)
 async def giv_cod(message: Message):
-    code_id_str = message.text
-    try:
-        code_id = int(code_id_str)
-    except ValueError:
-        await message.answer("Пожалуйста, введите числовой id.")
-        return
+    sponsor_access = await check_subscription(message.from_user.id)
+    if sponsor_access:
+        code_id_str = message.text
+        try:
+            code_id = int(code_id_str)
+        except ValueError:
+            await message.answer("Пожалуйста, введите числовой id.")
+            return
 
-    cod = await get_cods(code_id)
-    if cod:
-        await message.answer(f"Название: {cod.name}")
+        cod = await get_cods(code_id)
+        if cod:
+            await message.answer(f"Название: {cod.name}")
+        else:
+            await message.answer("Код с таким id не найден.")
     else:
-        await message.answer("Код с таким id не найден.")
+        sponsor = await get_sponsor()
+        await message.answer(f'подпишитесь на спосноров {sponsor.name}')
 
 
 async def send_advertisement(message: Message):
@@ -44,3 +55,14 @@ async def send_advertisement(message: Message):
         else:
             await message.answer(text)
             asyncio.create_task(send_advertisement(message))
+
+
+async def check_subscription(user_id: int):
+    try:
+        member = await bot.get_chat_member(chat_id=CHANNEL_USERNAME, user_id=user_id)
+        if member.status in ["member", "administrator", "creator"]:
+            return True
+        else:
+            return False
+    except TelegramBadRequest:  # Если чат не найден или есть ошибка
+        return False
--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 import asyncio
-import os
-from aiogram import Bot, Dispatcher
-from dotenv import load_dotenv
-
+from app.bot import bot, dp  # Импортируем бот и диспетчер
 from app.hundlers import router
 from app.database.models import async_main
 
 
 async def main():
-    load_dotenv()
     await async_main()
-    bot = Bot(token=os.getenv('TOKEN'))
-    dp = Dispatcher()
     dp.include_router(router)
-    await dp.start_polling(bot)
+    await dp.start_polling(bot)  # Запускаем бота
 
 if __name__ == "__main__":
     asyncio.run(main())
